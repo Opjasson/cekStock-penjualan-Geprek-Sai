f3eace9ba6cde82a650a854821f5387730df1aac
Opjasson <azmitheusos333@gmail.com>
2025-05-06 06:28:28 +0700

    frontend : add xample content in data menu

--- cekStockPenjualan_geprekSai/src/assets/index.js
+++ cekStockPenjualan_geprekSai/src/assets/index.js
@@ -1,5 +1,5 @@
-import logoSai from "./logoSai.png"
+import logoSai from "./logoSai.png";
+import menu1 from "./menu1.jpg";
+import menu2 from "./menu2.jpg";
 
-export {
-    logoSai
-}
+export { logoSai, menu1, menu2 };
--- cekStockPenjualan_geprekSai/src/pages/Home.jsx
+++ cekStockPenjualan_geprekSai/src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Kasir_Layout from "../components/mainLayout/Kasir_Layout";
 import { MdOutlineDataset } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { logoSai, menu1 } from "../assets";
 
 const Home = () => {
+    const [data, setData] = useState([]);
+    const [findLower, setfindLower] = useState("");
+
+    const navigate = useNavigate();
     return (
         <Kasir_Layout>
             <div className="flex">
@@ -11,6 +18,106 @@ const Home = () => {
                     <div className="flex items-center">
                         <MdOutlineDataset className="text-3xl" />
                         <h1>Data Menu</h1>
+                    </div>
+
+                    <div className="flex justify-between">
+                        <select name="" id="" className="border">
+                            <option value="">Semua kategori</option>
+                            <option value="minuman">Minuman</option>
+                            <option value="makanan">Makanan</option>
+                        </select>
+
+                        <div className="flex items-center bg-slate-300 lg:px-2 px-1 lg:py-1.5 py-0 lg:rounded-xl rounded-sm lg:w-80 w-[55%]">
+                            <input
+                                type="text"
+                                placeholder="Cari nama..."
+                                className="outline-none w-full text-sm"
+                                onChange={(e) => setfindLower(e.target.value)}
+                            />
+                            <FaMagnifyingGlass className="lg:text-2xl" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap justify-around mt-5">
+                        <div className="border w-48 mb-5 hover:cursor-pointer">
+                            <div className="border">
+                                <img
+                                    src={menu1}
+                                    alt=""
+                                    className="md:h-28 h-16 border mx-auto"
+                                />
+                            </div>
+
+                            <div className="text-center">
+                                <p>Nasi ayam special</p>
+                                <p>Rp.10.000</p>
+                                <p>Stock : 10x</p>
+                            </div>
+                        </div>
+
+                        <div className="border w-48 mb-5 hover:cursor-pointer">
+                            <div className="border">
+                                <img
+                                    src={menu1}
+                                    alt=""
+                                    className="md:h-28 h-16 border mx-auto"
+                                />
+                            </div>
+
+                            <div className="text-center">
+                                <p>Nasi ayam special</p>
+                                <p>Rp.10.000</p>
+                                <p>Stock : 10x</p>
+                            </div>
+                        </div>
+
+                        <div className="border w-48 mb-5 hover:cursor-pointer">
+                            <div className="border">
+                                <img
+                                    src={menu1}
+                                    alt=""
+                                    className="md:h-28 h-16 border mx-auto"
+                                />
+                            </div>
+
+                            <div className="text-center">
+                                <p>Nasi ayam special</p>
+                                <p>Rp.10.000</p>
+                                <p>Stock : 10x</p>
+                            </div>
+                        </div>
+
+                        <div className="border w-48 mb-5 hover:cursor-pointer">
+                            <div className="border">
+                                <img
+                                    src={menu1}
+                                    alt=""
+                                    className="md:h-28 h-16 border mx-auto"
+                                />
+                            </div>
+
+                            <div className="text-center">
+                                <p>Nasi ayam special</p>
+                                <p>Rp.10.000</p>
+                                <p>Stock : 10x</p>
+                            </div>
+                        </div>
+
+                        <div className="border w-48 mb-5 hover:cursor-pointer">
+                            <div className="border">
+                                <img
+                                    src={menu1}
+                                    alt=""
+                                    className="md:h-28 h-16 border mx-auto"
+                                />
+                            </div>
+
+                            <div className="text-center">
+                                <p>Nasi ayam special</p>
+                                <p>Rp.10.000</p>
+                                <p>Stock : 10x</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="border w-[34%]">
