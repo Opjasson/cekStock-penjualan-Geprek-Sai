06591ed847f65785b39b19f7a3225311205d5a9a
Opjasson <azmitheusos333@gmail.com>
2025-05-06 14:12:59 +0700

    frontend : done setting add barang in cart

--- cekStockPenjualan_geprekSai/src/pages/Home.jsx
+++ cekStockPenjualan_geprekSai/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Kasir_Layout from "../components/mainLayout/Kasir_Layout";
 import { MdOutlineDataset } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";
@@ -6,28 +6,53 @@ import { useNavigate } from "react-router-dom";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { logoSai, menu1 } from "../assets";
 
+const keranjang = [
+    {
+        id: 1,
+        nama: "ayam bakar",
+        image: menu1,
+        idr: 15.0,
+    },
+    {
+        id: 2,
+        nama: "ayam chicken",
+        image: menu1,
+        idr: 25.0,
+    },
+];
+
 const Home = () => {
     const [data, setData] = useState([]);
     const [findLower, setfindLower] = useState("");
-    const [cart, setCart] = useState([
-        {
-            no: 1,
-            nama: "Ayam chicken",
-            harga: 30000,
-        },
-        {
-            no: 2,
-            nama: "Pop ice",
-            harga: 20000,
-        },
-        {
-            no: 3,
-            nama: "Kentang",
-            harga: 25000,
-        },
-    ]);
+    const [qty, setQty] = useState(0);
+    const [cart, setCart] = useState([]);
 
     const navigate = useNavigate();
+
+    const addCart = (id) => {
+        if (cart.find((item) => item.id === id)) {
+            setCart(
+                cart.map((barang) =>
+                    barang.id === id
+                        ? { ...barang, qty: barang.qty + 1 }
+                        : barang
+                )
+            );
+        } else {
+            const masuk = keranjang.filter((e) => e.id === id);
+            console.log(masuk);
+            masuk.map((a) =>
+                setCart([...cart, { id, nama: a.nama, qty: 1, idr: a.idr }])
+            );
+        }
+    };
+
+    const onDelete = (id) => {
+        const keranjang = cart.filter((item) => item.id !== id);
+        setCart(keranjang);
+    };
+    console.log(cart);
+
     return (
         <Kasir_Layout>
             <div className="flex justify-between">
@@ -56,85 +81,26 @@ const Home = () => {
                     </div>
 
                     <div className="flex flex-wrap justify-around mt-5">
-                        <div className="border w-48 mb-5 hover:cursor-pointer">
-                            <div className="border">
-                                <img
-                                    src={menu1}
-                                    alt=""
-                                    className="md:h-28 h-16 border mx-auto"
-                                />
-                            </div>
+                        {keranjang.map((item) => (
+                            <div
+                                onClick={() => addCart(item.id)}
+                                className="border w-48 mb-5 hover:cursor-pointer"
+                                key={item.id}>
+                                <div className="border">
+                                    <img
+                                        src={item.image}
+                                        alt=""
+                                        className="md:h-28 h-16 border mx-auto"
+                                    />
+                                </div>
 
-                            <div className="text-center">
-                                <p>Nasi ayam special</p>
-                                <p>Rp.10.000</p>
-                                <p>Stock : 10x</p>
+                                <div className="text-center">
+                                    <p>{item.nama}</p>
+                                    <p>Rp.{item.idr}</p>
+                                    <p>Stock : 10x</p>
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="border w-48 mb-5 hover:cursor-pointer">
-                            <div className="border">
-                                <img
-                                    src={menu1}
-                                    alt=""
-                                    className="md:h-28 h-16 border mx-auto"
-                                />
-                            </div>
-
-                            <div className="text-center">
-                                <p>Nasi ayam special</p>
-                                <p>Rp.10.000</p>
-                                <p>Stock : 10x</p>
-                            </div>
-                        </div>
-
-                        <div className="border w-48 mb-5 hover:cursor-pointer">
-                            <div className="border">
-                                <img
-                                    src={menu1}
-                                    alt=""
-                                    className="md:h-28 h-16 border mx-auto"
-                                />
-                            </div>
-
-                            <div className="text-center">
-                                <p>Nasi ayam special</p>
-                                <p>Rp.10.000</p>
-                                <p>Stock : 10x</p>
-                            </div>
-                        </div>
-
-                        <div className="border w-48 mb-5 hover:cursor-pointer">
-                            <div className="border">
-                                <img
-                                    src={menu1}
-                                    alt=""
-                                    className="md:h-28 h-16 border mx-auto"
-                                />
-                            </div>
-
-                            <div className="text-center">
-                                <p>Nasi ayam special</p>
-                                <p>Rp.10.000</p>
-                                <p>Stock : 10x</p>
-                            </div>
-                        </div>
-
-                        <div className="border w-48 mb-5 hover:cursor-pointer">
-                            <div className="border">
-                                <img
-                                    src={menu1}
-                                    alt=""
-                                    className="md:h-28 h-16 border mx-auto"
-                                />
-                            </div>
-
-                            <div className="text-center">
-                                <p>Nasi ayam special</p>
-                                <p>Rp.10.000</p>
-                                <p>Stock : 10x</p>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
 
@@ -155,45 +121,29 @@ const Home = () => {
                             <p>List Keranjang</p>
                         </div>
 
-                        <div
-                            onClick={() => navigate(`Detail-stock/${item.id}`)}
-                            className="flex justify-between hover:bg-slate-300 px-2 py-1.5 text-sm font-extralight mt-2 border border-slate-400">
-                            <h2 className="pr-3.5 ">No</h2>
-                            <h2 className="w-32 ">Nama</h2>
-                            <h2 className="w-32 ">Qty</h2>
-                            <h2 className="w-32 ">Harga</h2>
+                        <div className="flex justify-between hover:bg-slate-300 px-2 py-1.5 text-sm font-extralight mt-2 border border-slate-400">
+                            <h2 className="pr-2">No</h2>
+                            <h2 className="w-32">Nama</h2>
+                            <h2 className="w-32">Qty</h2>
+                            <h2 className="w-32">Harga</h2>
                             <h2 className="px-1">#</h2>
                         </div>
 
-                        <div
-                            onClick={() => navigate(`Detail-stock/${item.id}`)}
-                            className="flex justify-between hover:bg-slate-300 px-2 py-3 text-sm font-extralight border border-slate-400">
-                            <h2 className="pr-3.5 ">No</h2>
-                            <h2 className="w-32 ">Nasi Ayam special</h2>
-                            <div className="pr-3 w-[7.5rem]">
-                                <input
-                                    className="w-full h-5 rounded-md border border-slate-300 pl-1"
-                                    type="number"
-                                />
-                            </div>
-                            <h2 className="w-32 ">Harga</h2>
-                            <h2 className="bg-red-500 hover:bg-red-600 hover:cursor-pointer px-1 rounded-sm text-white">
-                                x
-                            </h2>
-                        </div>
-
                         {cart.map((item) => (
-                            <div className="flex justify-between hover:bg-slate-300 px-2 py-3 text-sm font-extralight border border-slate-400">
-                                <h2 className="pr-3.5 ">{item.no}</h2>
+                            <div
+                                className="flex justify-between hover:bg-slate-300 px-2 py-3 text-sm font-extralight border border-slate-400"
+                                key={item.id}>
+                                <h2 className="pr-4.5 ">{item.id}</h2>
                                 <h2 className="w-32 ">{item.nama}</h2>
-                                <div className="pr-3 w-[7.5rem]">
-                                    <input
-                                        className="w-full h-5 rounded-md border border-slate-300 pl-1"
-                                        type="number"
-                                    />
-                                </div>
-                                <h2 className="w-32 ">{item.harga}</h2>
-                                <h2 className="bg-red-500 hover:bg-red-600 hover:cursor-pointer px-1 rounded-sm text-white">
+                                <h2 className="w-32">{item.qty}</h2>
+                                <h2 className="w-32 ">
+                                    {item.qty > 1
+                                        ? item.idr * item.qty
+                                        : item.idr}
+                                </h2>
+                                <h2
+                                    className="bg-red-500 hover:bg-red-600 hover:cursor-pointer px-1 rounded-sm text-white"
+                                    onClick={() => onDelete(item.id)}>
                                     x
                                 </h2>
                             </div>
