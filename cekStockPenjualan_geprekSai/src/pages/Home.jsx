3e5859e9e76368cd258362d2f4b8dcb787da64ec
Opjasson <azmitheusos333@gmail.com>
2025-05-06 23:05:52 +0700

    frontend : done setting kasir pages style

--- cekStockPenjualan_geprekSai/src/pages/Home.jsx
+++ cekStockPenjualan_geprekSai/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import Kasir_Layout from "../components/mainLayout/Kasir_Layout";
 import { MdOutlineDataset } from "react-icons/md";
 import { FaShoppingCart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 import { logoSai, menu1 } from "../assets";
+
 
 const keranjang = [
     {
@@ -19,6 +19,30 @@ const keranjang = [
         image: menu1,
         idr: 25000,
     },
+    {
+        id: 3,
+        nama: "ayam chicken",
+        image: menu1,
+        idr: 25000,
+    },
+    {
+        id: 4,
+        nama: "ayam chicken",
+        image: menu1,
+        idr: 25000,
+    },
+    {
+        id: 5,
+        nama: "ayam chicken",
+        image: menu1,
+        idr: 25000,
+    },
+    {
+        id: 6,
+        nama: "ayam chicken",
+        image: menu1,
+        idr: 25000,
+    },
 ];
 
 const Home = () => {
@@ -27,7 +51,7 @@ const Home = () => {
     const [qty, setQty] = useState(0);
     const [cart, setCart] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [cash, setCash] = useState()
+    const [cash, setCash] = useState();
 
     const navigate = useNavigate();
 
@@ -63,131 +87,146 @@ const Home = () => {
                 return acc + product.idr * item.qty;
             }, 0);
             setTotalPrice(sum);
-        }else{
-            setTotalPrice(0)
+        } else {
+            setTotalPrice(0);
         }
-    }, [cart])
-    
-    
-    
+    }, [cart]);
 
     return (
         <Kasir_Layout>
-            <div className="flex justify-between">
-                <div className="border w-4/6">
-                    <div className="flex items-center">
+            <div className="flex flex-wrap justify-between w-full">
+                {/* Session Data menu list */}
+                <div className="xl:w-4/6 mb-10 rounded-xl overflow-hidden bg-slate-100">
+                    {/* head session menu */}
+                    <div className="flex items-center bg-yellow-300 px-2.5 py-1.5 mb-3">
                         <MdOutlineDataset className="text-3xl" />
-                        <h1>Data Menu</h1>
+                        <h1 className="text-xl">Data Menu</h1>
                     </div>
+                    {/* end head */}
 
-                    <div className="flex justify-between">
-                        <select name="" id="" className="border">
-                            <option value="">Semua kategori</option>
-                            <option value="minuman">Minuman</option>
-                            <option value="makanan">Makanan</option>
-                        </select>
+                    {/* filter menu */}
+                    <select
+                        name=""
+                        id=""
+                        className="ml-3 w-56 py-2 bg-green-600 rounded-lg text-white">
+                        <option value="">Semua kategori</option>
+                        <option value="minuman">Minuman</option>
+                        <option value="makanan">Makanan</option>
+                    </select>
+                    {/* end filter menu */}
 
-                        <div className="flex items-center bg-slate-300 lg:px-2 px-1 lg:py-1.5 py-0 lg:rounded-xl rounded-sm lg:w-80 w-[55%]">
-                            <input
-                                type="text"
-                                placeholder="Cari nama..."
-                                className="outline-none w-full text-sm"
-                                onChange={(e) => setfindLower(e.target.value)}
-                            />
-                            <FaMagnifyingGlass className="lg:text-2xl" />
-                        </div>
-                    </div>
-
-                    <div className="flex flex-wrap justify-around mt-5">
+                    {/* Show all menu */}
+                    <div className="flex flex-wrap justify-around mt-12 relative">
                         {keranjang.map((item) => (
                             <div
                                 onClick={() => addCart(item.id)}
-                                className="border w-48 mb-5 hover:cursor-pointer"
+                                className="border w-48 mb-5 hover:cursor-pointer rounded-lg overflow-hidden hover:bg-slate-200 shadow-lg"
                                 key={item.id}>
-                                <div className="border">
+                                <div className="px-1.5 py-1.5">
                                     <img
                                         src={item.image}
                                         alt=""
-                                        className="md:h-28 h-16 border mx-auto"
+                                        className="md:h-28 h-16 border mx-auto w-full"
                                     />
                                 </div>
 
-                                <div className="text-center">
+                                <div className="text-center pb-2.5 font-bold">
                                     <p>{item.nama}</p>
-                                    <p>Rp.{item.idr}</p>
+                                    <p className="text-green-500">
+                                        Rp.{item.idr}
+                                    </p>
                                     <p>Stock : 10x</p>
                                 </div>
                             </div>
                         ))}
                     </div>
+                    {/* end show all menu */}
                 </div>
+                {/* end session menu list */}
 
-                <div className="border w-[30%]">
-                    <div className="flex items-center">
-                        <FaShoppingCart className="text-3xl" />
-                        <h1>Keranjang</h1>
+                {/* session keranjang */}
+                <div className="w-[24.9rem] rounded-xl overflow-hidden bg-slate-100 h-fit pb-10 xl:mx-0 mx-auto">
+                    {/* head session keranjang */}
+                    <div className="flex items-center bg-yellow-300 px-2.5 py-1.5 mb-3 gap-1.5">
+                        <FaShoppingCart className="text-2xl" />
+                        <h1 className="text-xl">Keranjang</h1>
                     </div>
+                    {/* end head session keranjang */}
 
-                    <div className="border flex justify-between">
-                        <label htmlFor="">ATAS NAMA :</label>
-                        <input type="text" className="border" />
+                    <div className="flex justify-between px-2 mb-5">
+                        <label htmlFor="">ATAS NAMA</label>
+                        <input type="text" className="border rounded-md" />
                     </div>
 
                     <div>
-                        <div className="flex">
-                            <FaShoppingCart />
-                            <p>List Keranjang</p>
+                        <div className="flex gap-2 px-1.5 items-center">
+                            <FaShoppingCart className="text-lg" />
+                            <p className="text-sm">List Keranjang</p>
                         </div>
 
-                        <div className="flex justify-between hover:bg-slate-300 px-2 py-1.5 text-sm font-extralight mt-2 border border-slate-400">
-                            <h2 className="pr-2">No</h2>
-                            <h2 className="w-32">Nama</h2>
-                            <h2 className="w-32">Qty</h2>
-                            <h2 className="w-32">Harga</h2>
-                            <h2 className="px-1">#</h2>
-                        </div>
-
-                        {cart.map((item) => (
-                            <div
-                                className="flex justify-between hover:bg-slate-300 px-2 py-3 text-sm font-extralight border border-slate-400"
-                                key={item.id}>
-                                <h2 className="pr-4.5 ">{item.id}</h2>
-                                <h2 className="w-32 ">{item.nama}</h2>
-                                <h2 className="w-32">{item.qty}</h2>
-                                <h2 className="w-32 ">
-                                    {item.qty > 1
-                                        ? item.idr * item.qty
-                                        : item.idr}
-                                </h2>
-                                <h2
-                                    className="bg-red-500 hover:bg-red-600 hover:cursor-pointer px-1 rounded-sm text-white"
-                                    onClick={() => onDelete(item.id)}>
-                                    x
-                                </h2>
+                        <div className="border-b-2 border-slate-400 pb-5">
+                            <div className="flex justify-between hover:bg-slate-300 px-2 py-1.5 text-sm font-extralight mt-2 border border-slate-400">
+                                <h2 className="pr-2">No</h2>
+                                <h2 className="w-32">Nama</h2>
+                                <h2 className="w-32">Qty</h2>
+                                <h2 className="w-32">Harga</h2>
+                                <h2 className="px-1">#</h2>
                             </div>
-                        ))}
 
-                        <div>
-                            <div className="border flex gap-1.5">
-                                <label htmlFor="" className="border">
+                            {cart.map((item) => (
+                                <div
+                                    className="flex justify-between hover:bg-slate-300 px-2 py-3 text-sm font-extralight border border-slate-400"
+                                    key={item.id}>
+                                    <h2 className="pr-4.5 ">{item.id}</h2>
+                                    <h2 className="w-32 ">{item.nama}</h2>
+                                    <h2 className="w-32">{item.qty}</h2>
+                                    <h2 className="w-32 ">
+                                        {item.qty > 1
+                                            ? item.idr * item.qty
+                                            : item.idr}
+                                    </h2>
+                                    <h2
+                                        className="bg-red-500 hover:bg-red-600 hover:cursor-pointer px-1 rounded-sm text-white"
+                                        onClick={() => onDelete(item.id)}>
+                                        x
+                                    </h2>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="px-2 py-2 flex flex-col gap-3">
+                            <div className=" flex gap-1.5 items-center">
+                                <label htmlFor="" className="">
                                     Total Bayar
                                 </label>
                                 <p>:</p>
-                                <input type="number" className="border" value={totalPrice} />
+                                <input
+                                    type="number"
+                                    className="bg-white rounded-sm p-1.5 shadow-md"
+                                    value={totalPrice}
+                                />
                             </div>
-                            <div className="border flex gap-1.5">
-                                <label htmlFor="" className="border pr-[46px]">
+                            <div className=" flex gap-1.5 items-center">
+                                <label htmlFor="" className=" pr-[46px]">
                                     Cash
                                 </label>
                                 <p>:</p>
-                                <input type="number" className="border" onChange={(e) => setCash(e.target.value)} />
+                                <input
+                                    type="number"
+                                    className="bg-white rounded-sm p-1.5 shadow-md"
+                                    onChange={(e) => setCash(e.target.value)}
+                                />
                             </div>
-                            <div className="border flex gap-1.5">
-                                <label htmlFor="" className="border pr-[5px]">
+                            <div className=" flex gap-1.5 items-center">
+                                <label htmlFor="" className=" pr-[5px]">
                                     Kembalian
                                 </label>
                                 <p>:</p>
-                                <input type="number" className="border" value={cash - totalPrice}/>
+                                <input
+                                    type="number"
+                                    className="bg-white rounded-sm p-1.5 shadow-md"
+                                    value={cash - totalPrice}
+                                />
                             </div>
                         </div>
 
@@ -201,6 +240,8 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+
+                {/* end session keranjang */}
             </div>
         </Kasir_Layout>
     );
