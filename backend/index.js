9dad9930f2948ca61efe0a6eacb66671058fe658
Opjasson <azmitheusos333@gmail.com>
2025-05-28 07:07:31 +0700

    backend : setting listen port true or false

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import menuRoute from "./routes/menuRoutes.js"
-import stockBahanRoute from "./routes/stokBahanRoutes.js"
+import menuRoute from "./routes/menuRoutes.js";
+import stockBahanRoute from "./routes/stokBahanRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -15,9 +15,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(menuRoute)
-app.use(stockBahanRoute)
+app.use(menuRoute);
+app.use(stockBahanRoute);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on server http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, (error) => {
+    if (error) {
+        console.log(error);
+    }else{
+        console.log(
+            `Server running on server http://localhost:${process.env.PORT}`
+        );
+    }
+    
 });
