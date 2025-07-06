7152c1fbe26bc08afebe2080638efcc79180e020
Opjasson <azmitheusos333@gmail.com>
2025-07-06 22:51:36 +0700

    backend : add route delete menu

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,4 +1,4 @@
-import { createMenu, getMenus, getMenuById, updateData_Menu } from "../controllers/menu.js";
+import { createMenu, getMenus, getMenuById, updateData_Menu, deleteMenu } from "../controllers/menu.js";
 import express from "express";
 
 const route = express.Router();
@@ -7,5 +7,6 @@ route.get("/menu", getMenus);
 route.get("/menu/:id", getMenuById);
 route.post("/menu", createMenu);
 route.patch("/menu/:id", updateData_Menu);
+route.delete("/menu/:id", deleteMenu);
 
 export default route;
