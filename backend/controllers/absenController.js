b0e0df00dd9bec6acdf211e0f0ae935cb0d1ccc5
Opjasson <azmitheusos333@gmail.com>
2025-08-23 00:59:01 +0700

    backend : add all controllers absen

---
+++ backend/controllers/absenController.js
@@ -0,0 +1,82 @@
+import absenModel from "../models/absenModel.js";
+
+export const createAbsen = async (req, res) => {
+    try {
+        const { jam_masuk, jam_keluar, tanggal, userId } = req.body;
+
+        await absenModel.create({
+            jam_masuk,
+            jam_keluar,
+            tanggal,
+            userId
+        });
+        res.status(201).json({ msg: "Data berhasil ditambahkan" });
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+};
+
+export const getAbsens = async (req, res) => {
+    try {
+        const response = await absenModel.findAll({
+            attributes: ["id", "jam_masuk", "jam_keluar", "tanggal", "userId"],
+        });
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+};
+
+export const getAbsenById = async (req, res) => {
+    try {
+        const response = await absenModel.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: ["id", "nama_menu", "harga", "kategori", "img"],
+        });
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+};
+
+export const updateData_Absen = async (req, res) => {
+    try {
+        const dataMenu = await absenModel.findOne({
+            where: {
+                id: req.params.id,
+            },
+        });
+        const { nama_menu, harga, kategori, stock_menu, img } = req.body;
+        await dataMenu.update(
+            {
+                nama_menu,
+                harga,
+                kategori,
+                img,
+            },
+            {
+                where: {
+                    id: dataMenu.id,
+                },
+            }
+        );
+        res.status(200).json({ msg: "data berhasil dirubah!" });
+    } catch (error) {
+        res.status(404).json({ msg: error.message });
+    }
+};
+
+export const deleteAbsen = async (req, res) => {
+    try {
+        absenModel.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json({ msg: "data berhasil dihapus!" });
+    } catch (error) {
+        res.status(404).json({ msg: error.message });
+    }
+};
