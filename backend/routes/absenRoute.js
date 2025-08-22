bb5080eddcd37df22585b59a92ac5638f9cd0c00
Opjasson <azmitheusos333@gmail.com>
2025-08-23 01:23:21 +0700

    backend : add updateData_Absen route

--- backend/routes/absenRoute.js
+++ backend/routes/absenRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createAbsen, getAbsens } from "../controllers/absenController.js";
+import { createAbsen, getAbsens, updateData_Absen } from "../controllers/absenController.js";
 
 const route = express.Router();
 
 route.get("/absen", getAbsens);
 route.post("/absen", createAbsen);
+route.patch("/absen/:id", updateData_Absen);
 
 export default route;
